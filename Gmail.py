77fced7b98902a6090e1a78d7346e63a85838699
L0NDONER <oflaherty@gmail.com>
2015-08-27 14:16:29 +0100

    Update Gmail.py
    Gmail.py v2
    imported netrc
    then from secret.py i've imported password, username
    and from address.py ive imported fromaddr, recipient

--- Gmail.py
+++ Gmail.py
@@ -1,53 +1,31 @@
-#!/usr/bin/python2
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import smtplib
-import hashlib
- 
-# Replace **** with your to and from addresses
+import netrc
+from secret import password, username #  Gmail password and username
+from address import fromaddr, recipient #  added addresses here because I can
 
-fromaddr = '****@gmail.com'
-toaddrs = '****@gmail.com'
+# Writing the message (You can write email in html format or plain text.)
 
-# Message of the email enclosed in three """
-# Speach marks so you can add plain text or HTML,
-# finishing with three Speach marks """
-# replace Yourname and 
-# Personsname with your own and recipiants
-#Replace **** with your gmail address
-#replace anyemailaddress with recipiants email address
-
-msg = """From: YourName <******@****>
-To: PersonsName <******@anyemailaddress>
-MIME-Version: 1.0
-Content-type: text/html
-Subject: SMTP HTML e-mail test.
+msg = """
+From: 
+To: 
+Subject:
 
 Hello World
-
-This is my first Mako Test
 """
 
-# Login Gmail Details
 
-username = '*****'  # Replce with your G username
-password = '****'  # Replace ******* with your Google Password
 
-# Hashlib Sha Cryptographic hashes and message digests.
-
-h = hashlib.sha224()
-h.update(msg)
-print h.hexdigest()  # Will print the Cryptographic key
-
-passwd = "mypass"
-passwd = bytes(passwd)
-passwd = hashlib.sha224("****").hexdigest()  # *** replace with password
-
-# SMTP_SSL SSL connection to Google
+#  Sending the mail
 
 server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-server_ssl.ehlo()  # optional, called by login()
+server_ssl.ehlo()  #  optional, called by login()
 server_ssl.login(username, password)
-server_ssl.sendmail(fromaddr, toaddrs, msg)
+server_ssl.sendmail(fromaddr, recipient, msg)
 server_ssl.quit()
-print 'successfully sent the Email'
+print('Sent email to %s' % recipient)
+
+
 
