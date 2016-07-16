619addad87b7c0c2b6bad5c643a90f9472e6df2d
L0NDONER <oflaherty@gmail.com>
2016-07-16 21:00:28 +0100

    Updated Gmail Python Script

--- Gmail.py
+++ Gmail.py
@@ -1,29 +1,29 @@
-#!/usr/bin/python
+##!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import email
-import re
+### Last updated July 16 2016
+### Martin O'Flaherty
+
 import smtplib
-import netrc
-from secret import password, username #  Gmail password and username
-from address import fromaddr, recipient #  added addresses here because I can
-from RawMail import msg, html #  added addresses here because I can
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from keys import username, password #  Gmail password and username more Secure 
+from emailaddress import From, To, Subject, body #  added addresses here because I can
 
-#  Record the MIME types of both parts - text/plain and text/html.
+email_text = """\  
+From: %s  
+To: %s  
+Subject: %s
 
-part1 = MIMEText(msg, 'plain')
-part2 = MIMEText(html, 'html')
+%s
+""" % (From, ", ".join(To), Subject, body)
 
 #  Sending the mail over Gmail ssl server
 
 server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
 server_ssl.ehlo()  #  optional, called by login()
 server_ssl.login(username, password)
-server_ssl.sendmail(fromaddr, recipient, msg)
+server_ssl.sendmail(From, To, Subject)
 server_ssl.quit()
-print "Your Email to %s was sent successfully" % (recipient,) #  .format seemed a better option here rather than % string
+print "Your Email to %s was sent successfully" % (From,) #  .format seemed a better option here rather than % string
 
 
 
