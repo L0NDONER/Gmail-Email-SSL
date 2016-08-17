72dc890bae79d4205d9869ab1bf77906174c344c
L0NDONER <oflaherty@gmail.com>
2016-08-17 20:48:30 +0100

    Double checked code with PEP8 online. Alot cleaner

--- Gmail.py
+++ Gmail.py
@@ -1,18 +1,22 @@
 ##!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-### Last updated July 17 2016
-### Martin O'Flaherty
+# Last updated July 17 2016
+# Martin O'Flaherty
 
 import smtplib
 from datetime import datetime
-from seceret import username, password #  Gmail password and username more Secure 
-from emailaddress import From, To, Subject, body #  added addresses here because I can
+
+# Gmail password and username more Secure
+from seceret import username, password
+
+# added addresses here because I can
+from emailaddress import From, To, Subject, body
 
 
-email_text = """\  
-From: %s  
-To: %s  
+email_text = """\
+From: %s
+To: %s
 Subject: %s
 
 %s
@@ -21,9 +25,9 @@ Subject: %s
 #  Sending the mail over Gmail ssl server
 
 server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-server_ssl.ehlo()  #  optional, called by login()
+server_ssl.ehlo()
 server_ssl.login(username, password)
 server_ssl.sendmail(From, To, Subject)
 server_ssl.quit()
-print "Your Email to %s was sent successfully" % (From,) # Format seemed a better option here rather than % string
+print "Your Email to %s was sent successfully" % (From,)
 print datetime.now().strftime("%d-%m-%y %H:%M:%S")
