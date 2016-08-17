af005b4c8723c976c0337e4a284dd2d7ceae1c07
L0NDONER <oflaherty@gmail.com>
2016-08-17 20:34:43 +0100

    Gmail Python script Version 3

--- Gmail.py
+++ Gmail.py
@@ -1,12 +1,14 @@
 ##!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-### Last updated July 16 2016
+### Last updated July 17 2016
 ### Martin O'Flaherty
 
 import smtplib
-from keys import username, password #  Gmail password and username more Secure 
+from datetime import datetime
+from seceret import username, password #  Gmail password and username more Secure 
 from emailaddress import From, To, Subject, body #  added addresses here because I can
+
 
 email_text = """\  
 From: %s  
@@ -24,6 +26,4 @@ server_ssl.login(username, password)
 server_ssl.sendmail(From, To, Subject)
 server_ssl.quit()
 print "Your Email to %s was sent successfully" % (From,) # Format seemed a better option here rather than % string
-
-
-
+print datetime.now().strftime("%d-%m-%y %H:%M:%S")
